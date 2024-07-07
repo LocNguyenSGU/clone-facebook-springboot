693de762a02f20bd5992b216b1f2e758ca21c862
LocNguyenSGU <lockbkbang@gmail.com>
2024-07-07 15:59:22 +0700

    Initial commit for Spring Boot backend

---
+++ src/test/java/com/example/clone_Facebook/CloneFacebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clone_Facebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneFacebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
